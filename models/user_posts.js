a85c9a76378a2844a794f79db019afa2577a63d0
Paul Brandenburg <kuba.brandenburg@poczta.fm>
2025-11-17 07:54:16 +0000

    zalążek_projekt02_todolatr

---
+++ models/user_posts.js
@@ -0,0 +1,14 @@
+const posts = [
+    "CRAZY RATS",
+    "Crazy? I was crazy once. They locked me in a room. A rubber room. A rubber room with rats. And rats make me crazy. Crazy? I was crazy once. They locked me in a room. A rubber room. A rubber room with rats. And rats make me crazy.",
+    "Lorem Ipsum",
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+    "Meowl is cool",
+    "I like the meowl"
+]
+
+export function getUserPosts() {
+
+}
+
+
